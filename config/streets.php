ecef8d7e02db2f8fee1c472e3968e405782a4f66
Oliver Lumby <me@oliverlumby.com>
2017-08-13 19:25:51 +0200

    build basic street lookup table

---
+++ config/streets.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    'map' => [
+
+        'avenida-del-cid' => [
+            'av. cid',
+            'avenida del cid',
+            'av cid',
+            'av .cid',
+            'avenida cid'
+        ],
+
+        'camino-de-picanya' => [
+            'cami picanya',
+            'camino de picanya',
+            'camino picanya'
+        ],
+
+        'avenida-de-los-hermanos-maristas' => [
+            'avenida de los hermanos maristas',
+            'av germans maristes',
+            'av hermanos maristas',
+            'germans maristes',
+            'hermanos maristas',
+            'av. germans maristes',
+            'av. hermanos maristas',
+            'av. de los germans maristas',
+            'av. de los hermanos maristas'
+        ],
+
+        'avenida-de-los-hermanos-machado' => [
+            'avenida de los hermanos machado',
+            'av germans machado',
+            'av hermanos machado',
+            'germans machado',
+            'hermanos machado',
+            'av. germans machado',
+            'av. hermanos machado',
+            'av. de los germans machado',
+            'av. de los hermanos machado'
+        ],
+
+    ]
+];
