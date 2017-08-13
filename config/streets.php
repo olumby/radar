01095e4beb074319462e6198d18639d018a8ae8d
Oliver Lumby <me@oliverlumby.com>
2017-08-13 22:16:08 +0200

    add more streets

--- config/streets.php
+++ config/streets.php
@@ -5,59 +5,108 @@ return [
     'map' => [
 
         'avenida-del-cid' => [
-            'av. cid',
+            'a.cid',
             'avenida del cid',
-            'av cid',
-            'av .cid',
             'avenida cid'
         ],
 
         'camino-de-picanya' => [
             'cami picanya',
             'camino de picanya',
-            'camino picanya'
+            'camino picanya',
+            'cami de picanya',
+            'picanya'
         ],
 
         'avenida-de-los-hermanos-maristas' => [
             'avenida de los hermanos maristas',
-            'av germans maristes',
-            'av hermanos maristas',
+            'a.germans maristes',
+            'a.hermanos maristas',
             'germans maristes',
             'hermanos maristas',
-            'av. germans maristes',
-            'av. hermanos maristas',
-            'av. de los germans maristas',
-            'av. de los hermanos maristas'
+            'a.de los germans maristas',
+            'a.de los hermanos maristas'
         ],
 
         'avenida-de-los-hermanos-machado' => [
             'avenida de los hermanos machado',
-            'av germans machado',
-            'av hermanos machado',
+            'a.germans machado',
+            'a.hermanos machado',
             'germans machado',
             'hermanos machado',
-            'av. germans machado',
-            'av. hermanos machado',
-            'av. de los germans machado',
-            'av. de los hermanos machado'
+            'a.de los germans machado',
+            'a.de los hermanos machado',
+            'antonio machado'
         ],
 
         'avenida-pio-baroja' => [
             'avenida pio baroja',
-            'av. pio baroja',
-            'av pio baroja',
-            'avgda pio baroja',
-            'avgda. pio baroja',
+            'a.pio baroja',
             'pio baroja'
         ],
 
         'avenida-ausias-march' => [
             'avenida ausias march',
-            'av. ausias march',
-            'av ausias march',
-            'avgda ausias march',
-            'avgda. ausias march',
-            'ausias march'
+            'a.ausias march',
+            'ausias march',
+            'a.march'
+        ],
+
+        'avenida-del-mestre-rodrigo' => [
+            'mestre rodrigo',
+            'avenida del mestre rodrigo',
+            'a.mestre rodrigo',
+            'm.rodrigo'
+        ],
+
+        'avenida-de-manuel-de-falla' => [
+            'avenida de manuel de falla',
+            'manuel de falla',
+            'a.manuel falla',
+            'a.manuel de falla',
+            'manuel falla',
+            'm.falla'
+        ],
+
+        'avenida-del-general-aviles' => [
+            'avenida del general aviles',
+            'a.general aviles',
+            'general aviles',
+            'g.aviles',
+            'gr.aviles'
+        ],
+
+        'paseo-de-la-pechina' => [
+            'passeig de la petxina',
+            'paseo de la pechina',
+            'pechina',
+            'petxina'
+        ],
+
+        'antonio-ferrandis' => [
+            'a.ferrandis',
+            'antonio ferrandis',
+            'a.antonio ferrandis'
+        ],
+
+        'avenida-dels-tarongers' => [
+            'tarongers',
+            'a.tarongers',
+            'avenida dels tarongers'
+        ],
+
+        'avenida-de-las-tres-cruces' => [
+            'avenguda de les tres creus',
+            'tres creus',
+            'avenida de las tres cruces',
+            'a.tres cruces',
+            'a.tres creus'
+        ],
+
+        'carretera-en-corts' => [
+            'c.en corts',
+            'carrera d\' en corts',
+            'carretera en corts'
         ]
     ]
 ];
