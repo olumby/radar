5774eb90b5c983063cce16c8f1c9d64eab7a4ac1
Oliver Lumby <me@oliverlumby.com>
2017-08-13 20:56:25 +0200

    seed data via twitter api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $tweets = collect(['895910011335004160', '895587871653978112', '895174512542666755', '894811999481278465', '894456800292065280', '893373524487933953', '893002880038424576', '892641722563428352', '892328310453174272', '891911006334660608', '890863107010158592', '890130180353777664', '889736885500874752', '889390291165339648']);
+
+        $tweets->each(function ($tweet) {
+            try {
+                $response = Twitter::getTweet($tweet, ['tweet_mode' => 'extended']);
+            } catch (Exception $e) {
+                dd(Twitter::logs());
+            }
+
+            return App\Tweet::create([
+                'twitter_id' => $response->id_str,
+                'text' => $response->full_text,
+                'raw_date' => $response->created_at
+            ]);
+        });
     }
 }
