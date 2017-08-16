8d08efdde1ad455036fd873f76714d8f72394e47
Oliver Lumby <me@oliverlumby.com>
2017-08-16 17:47:34 +0200

    use TweetFetcher class in latest command

--- app/Console/Commands/LatestTweetsCommand.php
+++ app/Console/Commands/LatestTweetsCommand.php
@@ -2,12 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Activity;
-use App\Support\Tweets\TweetParser;
-use App\Tweet;
-use Carbon\Carbon;
+use App\Support\Tweets\TweetFetcher;
 use Illuminate\Console\Command;
-use Thujohn\Twitter\Facades\Twitter;
 
 class LatestTweetsCommand extends Command
 {
@@ -42,53 +38,8 @@ class LatestTweetsCommand extends Command
      */
     public function handle()
     {
-        $this->comment('Fetching latest tweets..');
-        $latest = Activity::whereNotNull('latest_id')->latest()->first();
+        $mode =  $this->argument('mode');
 
-        $params = [
-            'q' => '#radarPLV',
-            'from' => 'policialocalvlc',
-            'result_type' => 'recent',
-            'count' => '20',
-            'tweet_mode' => 'extended'
-        ];
-
-        if ($latest) {
-            $params['since_id'] = $latest->latest_id;
-            $this->comment('Using "' . $latest->latest_id . '" as "since_id".');
-        }
-
-        try {
-            $response = Twitter::getSearch($params);
-        } catch (Exception $e) {
-            dd(Twitter::logs());
-        }
-
-        $this->comment('Found ' . count($response->statuses) . ' tweets..');
-
-        $tweets = collect($response->statuses)->reverse()->transform(function ($status) {
-            if (Tweet::where('twitter_id', $status->id_str)->count() == 0) {
-                $tweet = Tweet::create([
-                    'twitter_id' => $status->id_str,
-                    'text' => $status->full_text,
-                    'date' => Carbon::parse($status->created_at)->format('Y-m-d H:i:s')
-                ]);
-
-                $tweetParser = new TweetParser($tweet);
-                $tweetParser->setCommand($this)->parse();
-
-                return $tweet->twitter_id;
-            }
-
-            return null;
-        })->filter();
-
-        Activity::create([
-            'tweet_count' => $tweets->count(),
-            'latest_id' => $tweets->last(),
-            'mode' => $this->argument('mode')
-        ]);
-
-        $this->info('Finished');
+        (new TweetFetcher())->setCommand($this)->fetchLatestTweets(true, $mode);
     }
 }
--- app/Support/Tweets/TweetFetcher.php
+++ app/Support/Tweets/TweetFetcher.php
@@ -13,8 +13,63 @@ class TweetFetcher
 {
     use CommandTrait;
 
-    public function fetchLatestTweets($parse = true)
+    public function fetchLatestTweets($parse = true, $mode = 'manual')
     {
+        $this->report('Fetching latest tweets..', 'comment');
+        $latest = Activity::whereNotNull('latest_id')->latest()->first();
+
+        $params = [
+            'q' => '#radarPLV',
+            'from' => 'policialocalvlc',
+            'result_type' => 'recent',
+            'count' => '20',
+            'tweet_mode' => 'extended'
+        ];
+
+        if ($latest) {
+            $params['since_id'] = $latest->latest_id;
+            $this->report('Using "' . $latest->latest_id . '" as "since_id".', 'comment');
+        }
+
+        try {
+            $response = Twitter::getSearch($params);
+        } catch (Exception $e) {
+            dd(Twitter::logs());
+        }
+
+        $this->report('Found ' . count($response->statuses) . ' tweets..', 'comment');
+
+        $tweets = collect($response->statuses)->reverse()->transform(function ($status) use ($parse) {
+            if (Tweet::where('twitter_id', $status->id_str)->count() == 0) {
+                $tweet = Tweet::create([
+                    'twitter_id' => $status->id_str,
+                    'text' => $status->full_text,
+                    'date' => Carbon::parse($status->created_at)->format('Y-m-d H:i:s')
+                ]);
+
+                if ($parse) {
+                    $tweetParser = new TweetParser($tweet);
+
+                    if (isset($this->command)) {
+                        $tweetParser->setCommand($this->command);
+                    }
+
+                    $tweetParser->parse();
+                }
+
+                return $tweet->twitter_id;
+            }
+
+            return null;
+        })->filter();
+
+        Activity::create([
+            'tweet_count' => $tweets->count(),
+            'latest_id' => $tweets->last(),
+            'mode' => $mode
+        ]);
+
+        $this->report('Finished', 'info');
     }
 
     public function fetchTweets($ids, $parse = true, $mode = 'seed')
