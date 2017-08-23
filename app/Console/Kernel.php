da3103e894dd483c18c7ff4c4efe865fd2001493
Oliver Lumby <me@oliverlumby.com>
2017-08-23 20:54:00 +0200

    schedule latest command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\LatestTweetsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,8 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(LatestTweetsCommand::class)
+                 ->hourly()
+                 ->unlessBetween('20:00', '6:00');
     }
 
     /**
