b2e146a7ec8b3013dc934cb38eb463c3d8fa1f2c
Oliver Lumby <me@oliverlumby.com>
2017-09-19 20:25:50 +0200

    testing out using similar_text for street recognition

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,37 @@ $router->get('tweets', 'TweetController@index');
 $router->get('tweet/{tweet}', 'TweetController@show');
 
 $router->get('admin', 'AdminController@index');
+
+
+$router->get('tests', function () {
+    return [
+        do_street_compare('gral avilés'),
+        do_street_compare('a. march'),
+        do_street_compare('naranjos'),
+        do_street_compare('tres creus')
+    ];
+});
+
+
+function do_street_compare($text)
+{
+    $streets = collect(array_keys(config('streets.map')))->transform(function ($value) use ($text) {
+        $find = ['avenida', 'dels', 'del', 'los', 'de', 'las', 'la', 'calle', 'carretera', 'paseo', 'camino'];
+
+        $value = collect(explode('-', $value))->filter(function ($value) use ($find) {
+            return !in_array($value, $find);
+        })->toArray();
+
+        $value = implode(' ', $value);
+        similar_text($value, $text, $percent);
+
+        $new = [];
+        $new['text'] = $text;
+        $new['street'] = $value;
+        $new['percent'] = $percent;
+        
+        return $new;
+    })->sortByDesc('percent');
+
+    return $streets->values()->first();
+}
